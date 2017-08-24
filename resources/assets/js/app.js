540fb70922352871212ad591937adc88776001de
epona <muraka3tada4@icloud.com>
2017-08-25 03:38:47 +0900

    element-theme, logos, inpranted button

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,23 +1,14 @@
 // require('./bootstrap');
 
 window.Vue = require('vue');
-
-// Vue.component('compo-sample', {
-//   template: '<li>{{ samp.text }}</li>',
-//   props: ['samp']
-// });
-
-// import Vue from 'vue'
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/ja'
-import 'element-ui/lib/theme-default/index.css'
-// import App from './components/App'
+import 'element-theme-default/theme/index.css'
 
 Vue.use(ElementUI, {locale})
 
 const app = new Vue({
     el: '#app',
-    // components: { App },
     data: {
     },
     methods: {
--- resources/views/mainboard.blade.php
+++ resources/views/mainboard.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>jorro</title>
   <link rel="stylesheet" href="css/app.css"/>
   <script type="text/javascript">
@@ -12,36 +13,65 @@
 </head>
 <body>
 <div id="app">
-  <el-color-picker></el-color-picker>
+  <header id="main-header" class="header-wrapper">
+    <el-row :gutter="4" :type="flex" align="middle">
+      <el-col :xs="16" :sm="14" :md="{span: 2, offset: 2}">
+        <h1>
+          <img id="logo" src="img/logo-gray.png">
+        </h1>
+      </el-col>
 
+      <el-col :xs="4" :sm="6" :md="14">
+          <nav id="main-navbar" class="navbar">
+            <ul class="clearfix flex">
+              <li>
+                <a href="#about">
+                  <i class="fa fa-fw fa-commenting-o"></i>
+                </a>
+                ABOUT
+              <li>
+                <a href="#illusts">
+                  <i class="fa fa-fw fa-picture-o"></i>
+                </a>
+                ILLUSTS
+              <li>
+                <a href="#comics">
+                  <i class="fa fa-fw fa-book"></i>
+                </a>
+                COMICS
+              <li>
+                <a href="#photos">
+                  <i class="fa fa-fw fa-camera-retro"></i>
+                </a>
+                PHOTOS
+              <li>
+                <a href="#apps">
+                  <i class="fa fa-fw fa-apple"></i>
+                </a>
+                APPS
+          </nav>
+      </el-col>
 
-  <!--
-    <img id="logo" src="img/logo.png">
-
-    <nav>
-      <ul class="clearfix flex">
-        <li>
-          <a href="#about">
-            <i class="fa fa-fw fa-commenting-o"></i> ABOUT
-          </a>
-        <li>
-          <a href="#about">
-            <i class="fa fa-fw fa-picture-o"></i> ILLUSTS
-          </a>
-        <li>
-          <a href="#about">
-            <i class="fa fa-fw fa-book"></i> COMICS
-          </a>
-        <li>
-          <a href="#about">
-            <i class="fa fa-fw fa-camera-retro"></i> PHOTOS
-          </a>
-        <li>
-          <a href="#about">
-            <i class="fa fa-fw fa-apple"></i> APPS
-          </a>
-    </nav>
-  -->
+      <el-col :xs="4" :sm="4" :md="4">
+        <nav id="accounts-navbar" class="navbar">
+          <ul class="clearfix">
+            <li>
+              <a href="#twitter">
+                <i class="fa fa-fw fa-twitter"></i>
+              </a>
+            <li>
+              <a href="#instagram">
+                <i class="fa fa-fw fa-instagram"></i>
+              </a>
+            <li>
+              <a href="#github">
+                <i class="fa fa-fw fa-github-alt"></i>
+              </a>
+          </ul>
+        </nav>
+      </el-col>
+    </el-row>
+  </header>
 </div>
 
 <script src="js/app.js"></script>
